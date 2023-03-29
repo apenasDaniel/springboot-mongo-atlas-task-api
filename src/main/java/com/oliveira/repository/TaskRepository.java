d7ac56241f66f0021d3ed952ede5c063cd6bd140
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2023-03-29 18:23:26 -0300

    service findall, byid, update and delete

--- src/main/java/com/oliveira/repository/TaskRepository.java
+++ src/main/java/com/oliveira/repository/TaskRepository.java
@@ -1,4 +1,14 @@
 package com.oliveira.repository;
 
-public interface TaskRepository {
+import com.oliveira.model.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+    List<Task> findBySeverity(int severity);
+
+    @Query("{assignee: ?0} ")
+    List<Task> getTasksByAssignee(String assignee);
 }
---
+++ src/main/java/com/oliveira/service/TaskService.java
@@ -0,0 +1,53 @@
+package com.oliveira.service;
+
+import com.oliveira.model.Task;
+import com.oliveira.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task addTask(Task task) {
+        task.setTaskId(UUID.randomUUID().toString().split("-")[0]);
+        return taskRepository.save(task);
+    }
+
+    public List<Task> findAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskByTaskId(String taskId) {
+        return taskRepository.findById(taskId).get();
+    }
+
+    public List<Task> getTaskBySeverity(int severity) {
+        return taskRepository.findBySeverity(severity);
+    }
+
+    public List<Task> getTaskByAssignee(String assignee) {
+        return taskRepository.getTasksByAssignee(assignee);
+    }
+
+    public Task updateTask(Task taskRequest) {
+        //trazendo o documento do banco de dados
+        Task existingTask = taskRepository.findById(taskRequest.getTaskId()).get();
+        existingTask.setDescription(taskRequest.getDescription());
+        existingTask.setSeverity(taskRequest.getSeverity());
+        existingTask.setAssignee(taskRequest.getAssignee());
+        existingTask.setStoryPoint(taskRequest.getStoryPoint());
+        return taskRepository.save(existingTask);
+    }
+
+    public String deleteTask(String taskId) {
+        taskRepository.deleteById(taskId);
+        return taskId+" task deleted from dashboard ";
+    }
+
+}

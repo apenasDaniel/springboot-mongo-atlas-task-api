c0229e4118eefb23ef08833cf07046f24857d460
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2023-03-29 18:52:23 -0300

    controller getall, getbyid, update and delete

--- src/main/java/com/oliveira/controller/TaskController.java
+++ src/main/java/com/oliveira/controller/TaskController.java
@@ -1,9 +1,54 @@
 package com.oliveira.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.oliveira.model.Task;
+import com.oliveira.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        return taskService.addTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return taskService.findAllTasks();
+    }
+
+    @GetMapping("/{taskId}")
+    public Task getTask(@PathVariable String taskId) {
+        return taskService.getTaskByTaskId(taskId);
+    }
+
+    @GetMapping("/severity/{taskId}")
+    public List<Task> findTasksUsingSeverity(@PathVariable int severity) {
+        return taskService.getTaskBySeverity(severity);
+    }
+
+    @GetMapping("/assignee/{assignee}")
+    public List<Task> getTaskByAssignee(String assignee) {
+        return taskService.getTaskByAssignee(assignee);
+    }
+
+    @PutMapping
+    public Task modifyTask(Task task) {
+        return taskService.updateTask(task);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public String deleteTask(String taskId) {
+        return taskService.deleteTask(taskId);
+    }
+
 }
